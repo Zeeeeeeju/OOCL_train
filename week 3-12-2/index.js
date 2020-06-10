f02a5dccb4384658ca62f5fc2e9e0b0d0e1537a8
Zzeju <362119931@qq.com>
2020-06-10 13:03:31 +0800

    Finish Introduction to Object

---
+++ week 3-12-2/index.js
@@ -0,0 +1,15 @@
+// Create a null object and set its variable name as user;
+var user = {}
+console.log(user)
+// Add a name attribute and set its value as John;
+user['name'] = 'John'
+console.log(user)
+// Add a surname attribute and set its value as Mike;
+user['surname'] = 'Mike'
+console.log(user)
+// Modify the value of name attribute as Peter;
+user['name'] = 'Peter'
+console.log(user)
+// Delete name attribute of user.
+delete user.name
+console.log(user)
